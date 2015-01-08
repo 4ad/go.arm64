f166d6c5a427ee3c244b735e19288ca8ef2061f7
Dave Cheney <dave@cheney.net>
2015-01-08 23:24:06 +0000

    runtime: add fastrand1 defintion

--- src/runtime/asm_arm64.s
+++ src/runtime/asm_arm64.s
@@ -10,3 +10,34 @@
 TEXT runtime·rt0_go(SB),NOSPLIT,$0-0
 	BL	main·main(SB)
 	RETURN
+
+TEXT runtime·fastrand1(SB),NOSPLIT,$-8-4
+	MOVW	g_m(g), R1
+	MOVW	m_fastrand(R1), R0
+	ADDS	R0, R0
+	BGE	notneg
+	EOR	$0x88888eef, R0
+notneg:
+	MOVW	R0, m_fastrand(R1)
+	MOVW	R0, ret+0(FP)
+	RETURN
+
+TEXT runtime·memeq(SB),NOSPLIT,$-8-25
+	MOV	a+0(FP), R1
+	MOV	b+8(FP), R2
+	MOV	size+16(FP), R3
+	ADD	R1, R3, R6
+	MOV	$1, R0
+	MOV	R0, ret+24(FP)
+loop:
+	CMP	R1, R6
+	BEQ	done
+	MOVB	(R1)1!, R4
+	MOVB	(R2)1!, R5
+	CMP	R4, R5
+	BEQ	loop
+
+	MOVW	$0, R0
+	MOVB	R0, ret+24(FP)
+done:
+	RETURN
