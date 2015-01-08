68151900711a5790a9cea555aab2818d323796b5
Dave Cheney <dave@cheney.net>
2015-01-08 23:24:06 +0000

    runtime: add fastrand1 defintion
    Don't merge til we assign a `g` register

--- src/runtime/asm_arm64.s
+++ src/runtime/asm_arm64.s
@@ -10,3 +10,14 @@
 TEXT runtime·rt0_go(SB),NOSPLIT,$0-0
 	BL	main·main(SB)
 	RETURN
+
+TEXT runtime·fastrand1(SB),NOSPLIT,$-8-4
+	MOVW	g_m(g), R1
+	MOVW	m_fastrand(R1), R0
+	ADDS	R0, R0
+	B.GE	notneg
+	EOR	$0x88888eef, R0
+notneg:
+	MOVW	R0, m_fastrand(R1)
+	MOVW	R0, ret+0(FP)
+	RETURN
