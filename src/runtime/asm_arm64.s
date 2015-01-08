30567a9fb8506b5dac776b41af2c13f6507caacc
Aram Hăvărneanu <aram@mgk.ro>
2015-01-08 15:13:49 +0100

    runtime: work around linker bug
    Change-Id: Idd3bcb95f0fd80df66b67a41a6a11be34c423d40

--- src/runtime/asm_arm64.s
+++ src/runtime/asm_arm64.s
@@ -1,7 +1,13 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 #include "go_asm.h"
 #include "go_tls.h"
 #include "funcdata.h"
 #include "textflag.h"
 
 TEXT runtime·rt0_go(SB),NOSPLIT,$0
-        RET
+	MOV	$42, R4
+	MOV R4, (R4)	// crash
+	RET
--- src/runtime/rt0_linux_arm64.s
+++ src/runtime/rt0_linux_arm64.s
@@ -1,13 +1,41 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 #include "textflag.h"
 
-TEXT _rt0_arm64_linux(SB),NOSPLIT,$0
-	B _main<>(SB)
-
-TEXT _main<>(SB),NOSPLIT,$-8
-	MOV 0(SP), R3 // argc
-	ADD $8, SP, R4 // argv
-	B main(SB)
+TEXT _rt0_arm64_linux(SB),NOSPLIT,$-8
+	// reference the dummy symbols so that they end up in the binary.
+	MOVW	$dummydata(SB), R11
+	MOVW	$dummyrodata(SB), R11
+	MOVW	$dummynoptrdata(SB), R11
+	MOVW	$dummybss(SB), R11
+	MOVW	$dummynoptrbss(SB), R11
+	MOV	0(SP), R0	// argc
+	MOV	$8(SP), R1	// argv
+	B	main(SB)
 
 TEXT main(SB),NOSPLIT,$-8
-	MOV	$runtime·rt0_go(SB), R0
-	B	(R0)
+	MOV	$runtime·rt0_go(SB), R2
+	B	(R2)
+
+// Linker has a bug, and we need non-zero length symbols in
+// these sections.
+
+// .data
+DATA dummydata(SB)/4, $2
+GLOBL dummydata(SB), 0, $4
+
+// .rodata
+DATA dummyrodata(SB)/4, $1
+GLOBL dummyrodata(SB), RODATA, $4
+
+// .noptrdata
+DATA dummynoptrdata(SB)/4, $3
+GLOBL dummynoptrdata(SB), NOPTR, $4
+
+// .bss
+GLOBL dummybss(SB), 0, $4
+
+// .noptrbss
+GLOBL dummynoptrbss(SB), NOPTR, $4
