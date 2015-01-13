72f159d2c6d282145a99caa159d9766493ee3898
Michael Hudson-Doyle <michael.hudson@canonical.com>
2015-01-13 15:26:15 +1300

    use post increment form when copying forward

--- src/runtime/memmove_arm64.s
+++ src/runtime/memmove_arm64.s
@@ -6,7 +6,6 @@
 
 // void runtime·memmove(void*, void*, uintptr)
 TEXT runtime·memmove(SB), NOSPLIT, $-8-24
-        WORD    $0x12345678
 	MOV	to+0(FP), R3
 	MOV	from+8(FP), R4
 	MOV	n+16(FP), R5
@@ -18,12 +17,10 @@ check:
 	CMP	R3, R4
 	BGT	backward
 
-	SUB	$1, R3
 	ADD	R3, R5
-	SUB	$1, R4
 loop:
-	MOVBU	1(R4)!, R6
-	MOVBU	R6, 1(R3)!
+	MOVBU	(R4)1!, R6
+	MOVBU	R6, (R3)1!
 	CMP	R3, R5
 	BNE	loop
 	RETURN
@@ -36,5 +33,4 @@ loop1:
 	MOVBU	R6, -1(R5)!
 	CMP	R3, R5
 	BNE	loop1
-        WORD    $0x87654321
 	RETURN
