66c38911b9e9201ab09f1742fb62374b9685500b
Aram Hăvărneanu <aram@mgk.ro>
2015-01-20 18:00:57 +0100

    runtime: add cgocallback

--- src/runtime/asm_arm64.s
+++ src/runtime/asm_arm64.s
@@ -195,6 +195,123 @@ loop:
 done:
 	RETURN
 
+// cgocallback(void (*fn)(void*), void *frame, uintptr framesize)
+// Turn the fn into a Go func (by taking its address) and call
+// cgocallback_gofunc.
+TEXT runtime·cgocallback(SB),NOSPLIT,$24-24
+	MOV	$fn+0(FP), R3
+	MOV	R3, 8(SP)
+	MOV	frame+8(FP), R3
+	MOV	R3, 16(SP)
+	MOV	framesize+16(FP), R3
+	MOV	R3, 24(SP)
+	MOV	$runtime·cgocallback_gofunc(SB), R3
+	BL	(R3)
+	RETURN
+
+// cgocallback_gofunc(FuncVal*, void *frame, uintptr framesize)
+// See cgocall.c for more details.
+TEXT ·cgocallback_gofunc(SB),NOSPLIT,$16-24
+	NO_LOCAL_POINTERS
+
+	// Load m and g from thread-local storage.
+	MOVB	runtime·iscgo(SB), R3
+	CMP	$0, R3 
+	BEQ	nocgo
+	BL	runtime·load_g(SB)
+nocgo:
+
+	// If g is nil, Go did not create the current thread.
+	// Call needm to obtain one for temporary use.
+	// In this case, we're running on the thread stack, so there's
+	// lots of space, but the linker doesn't know. Hide the call from
+	// the linker analysis by using an indirect call.
+	CMP	$0, g
+	BNE	havem
+	MOV	g, savedm-8(SP) // g is zero, so is m.
+	MOV	$runtime·needm(SB), R3
+	BL	(R3)
+
+	// Set m->sched.sp = SP, so that if a panic happens
+	// during the function we are about to execute, it will
+	// have a valid SP to run on the g0 stack.
+	// The next few lines (after the havem label)
+	// will save this SP onto the stack and then write
+	// the same SP back to m->sched.sp. That seems redundant,
+	// but if an unrecovered panic happens, unwindm will
+	// restore the g->sched.sp from the stack location
+	// and then systemstack will try to use it. If we don't set it here,
+	// that restored SP will be uninitialized (typically 0) and
+	// will not be usable.
+	MOV	g_m(g), R3
+	MOV	m_g0(R3), R3
+	MOV	R1, (g_sched+gobuf_sp)(R3)
+
+havem:
+	MOV	g_m(g), R8
+	MOV	R8, savedm-8(SP)
+	// Now there's a valid m, and we're running on its m->g0.
+	// Save current m->g0->sched.sp on stack and then set it to SP.
+	// Save current sp in m->g0->sched.sp in preparation for
+	// switch back to m->curg stack.
+	// NOTE: unwindm knows that the saved g->sched.sp is at 8(R1) aka savedsp-16(SP).
+	MOV	m_g0(R8), R3
+	MOV	(g_sched+gobuf_sp)(R3), R4
+	MOV	R4, savedsp-16(SP)
+	MOV	R1, (g_sched+gobuf_sp)(R3)
+
+	// Switch to m->curg stack and call runtime.cgocallbackg.
+	// Because we are taking over the execution of m->curg
+	// but *not* resuming what had been running, we need to
+	// save that information (m->curg->sched) so we can restore it.
+	// We can restore m->curg->sched.sp easily, because calling
+	// runtime.cgocallbackg leaves SP unchanged upon return.
+	// To save m->curg->sched.pc, we push it onto the stack.
+	// This has the added benefit that it looks to the traceback
+	// routine like cgocallbackg is going to return to that
+	// PC (because the frame we allocate below has the same
+	// size as cgocallback_gofunc's frame declared above)
+	// so that the traceback will seamlessly trace back into
+	// the earlier calls.
+	//
+	// In the new goroutine, -16(SP) and -8(SP) are unused.
+	MOV	m_curg(R8), g
+	BL	runtime·save_g(SB)
+	MOV	(g_sched+gobuf_sp)(g), R4 // prepare stack as R4
+	MOV	(g_sched+gobuf_pc)(g), R5
+	MOV	R5, -24(R4)
+	MOV	$-24(R4), R0
+	MOV	R0, SP
+	BL	runtime·cgocallbackg(SB)
+
+	// Restore g->sched (== m->curg->sched) from saved values.
+	MOV	0(SP), R5
+	MOV	R5, (g_sched+gobuf_pc)(g)
+	MOV	$24(SP), R4
+	MOV	R4, (g_sched+gobuf_sp)(g)
+
+	// Switch back to m->g0's stack and restore m->g0->sched.sp.
+	// (Unlike m->curg, the g0 goroutine never uses sched.pc,
+	// so we do not have to restore it.)
+	MOV	g_m(g), R8
+	MOV	m_g0(R8), g
+	BL	runtime·save_g(SB)
+	MOV	(g_sched+gobuf_sp)(g), R1
+	MOV	savedsp-16(SP), R4
+	MOV	R4, (g_sched+gobuf_sp)(g)
+
+	// If the m on entry was nil, we called needm above to borrow an m
+	// for the duration of the call. Since the call is over, return it with dropm.
+	MOV	savedm-8(SP), R6
+	CMP	$0, R6
+	BNE	droppedm
+	MOV	$runtime·dropm(SB), R3
+	BL	(R3)
+droppedm:
+
+	// Done!
+	RET
+
 TEXT runtime·getcallerpc(SB),NOSPLIT,$-8-16
 	MOV	0(SP), R0
 	MOV	R0, ret+8(FP)
