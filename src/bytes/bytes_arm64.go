b6533a2655efbc60fa45e6afe33cbebfaef9b21e
Dave Cheney <dave@cheney.net>
2015-01-30 03:51:13 +0000

    bytes: add slow impl of Index and Equal
    These should call in the runtime asm versions, but this can be done later.
    
    These versions come from the portable versions from bytes_test.go so are assumed to be well tested.

--- src/bytes/bytes_arm64.go
+++ src/bytes/bytes_arm64.go
@@ -1,22 +1,33 @@
-// Copyright 2010 The Go Authors.  All rights reserved.
+// Copyright 2015 The Go Authors.  All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !arm64
-
 package bytes
 
-//go:noescape
-
 // IndexByte returns the index of the first instance of c in s, or -1 if c is not present in s.
-func IndexByte(s []byte, c byte) int // ../runtime/asm_$GOARCH.s
-
-//go:noescape
+func IndexByte(s []byte, c byte) int {
+	for i, b := range s {
+		if b == c {
+			return i
+		}
+	}
+	return -1
+}
 
 // Equal returns a boolean reporting whether a and b
 // are the same length and contain the same bytes.
 // A nil argument is equivalent to an empty slice.
-func Equal(a, b []byte) bool // ../runtime/asm_$GOARCH.s
+func Equal(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, c := range a {
+		if c != b[i] {
+			return false
+		}
+	}
+	return true
+}
 
 //go:noescape
 
