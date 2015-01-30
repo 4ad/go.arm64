e4a74bad316f6e8abaca715c01d929380cdc2716
Dave Cheney <dave@cheney.net>
2015-01-30 12:57:47 +0000

    math: use FABSD rather than bit twiddling

--- src/math/abs_arm64.s
+++ src/math/abs_arm64.s
@@ -5,8 +5,7 @@
 #include "textflag.h"
 
 TEXT ·Abs(SB),NOSPLIT,$0-16
-	MOV	x+0(FP), R3
-	MOV 	$((1<<63)-1), R4
-	AND	R4, R3
-	MOV	R3, ret+8(FP)
+	FMOVD	x+0(FP), F3
+	FABSD	F3, F3
+	FMOVD	F3, ret+8(FP)
 	RETURN
