0f944f7101ffe95e182e89ff8a9ef8539a57801c
Dave Cheney <dave@cheney.net>
2015-02-03 14:25:29 +1100

    third times the charm

--- src/cmd/7g/reg.c
+++ src/cmd/7g/reg.c
@@ -1241,8 +1241,7 @@ RtoB(int r)
 {
 	if((r >= REGMIN) && (r <= REGMAX))
 		return 1ULL << r;
-	r += 32;
-	if((r >= FREGMIN) && (r <= FREGMAX))
+	if((r >= (NREG + FREGMIN)) && (r <= (NREG+FREGMAX)))
 		return 1ULL << r;
 	return 0;
 }
