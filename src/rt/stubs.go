c518357f47b21f76f729506065972986e9303ebb
minux <minux@golang.org>
2015-02-09 09:10:59 +0000

    rt: add prototypes for assembly functions

--- src/rt/stubs.go
+++ src/rt/stubs.go
@@ -18,3 +18,22 @@ func add(p unsafe.Pointer, x uintptr) unsafe.Pointer {
 }
 
 func getg() *g
+
+// defined in sys_$GOOS_$GOARCH.s
+func read(fd int32, p unsafe.Pointer, n int32) int32
+func close(fd int32) int32
+
+func exit(code int32)
+func nanotime() int64
+func usleep(usec uint32)
+
+func mmap(addr unsafe.Pointer, n uintptr, prot, flags, fd int32, off uint32) unsafe.Pointer
+func munmap(addr unsafe.Pointer, n uintptr)
+
+//go:noescape
+func write(fd uintptr, p unsafe.Pointer, n int32) int32
+
+//go:noescape
+func open(name *byte, mode, perm int32) int32
+
+func madvise(addr unsafe.Pointer, n uintptr, flags int32)
