2b3de976316bfe37aa1d9662e6da847b84361292
Aram Hăvărneanu <aram@mgk.ro>
2015-02-27 18:40:22 +0100

    cmd/internal/obj/arm64: disable XPRE/XPOST cases in optab

--- src/cmd/internal/obj/arm64/asm7.go
+++ src/cmd/internal/obj/arm64/asm7.go
@@ -2240,35 +2240,36 @@ func asmout(ctxt *obj.Link, p *obj.Prog, o *Optab, out []uint32) {
 			o1 = olsr12u(ctxt, int32(opldr12(ctxt, int(p.As))), v, r, int(p.To.Reg))
 		}
 
-	case 22: /* movT (R)O!,R; movT O(R)!, R -> ldrT */
-		v = int32(p.From.Offset)
-
-		if v < -256 || v > 255 {
-			ctxt.Diag("offset out of range\n%v", p)
-		}
-		o1 = opldrpp(ctxt, int(p.As))
-		if p.From.Type == D_XPOST {
-			o1 |= 1 << 10
-		} else {
-
-			o1 |= 3 << 10
-		}
-		o1 |= ((uint32(v) & 0x1FF) << 12) | (uint32(p.From.Reg) << 5) | uint32(p.To.Reg)
-
-	case 23: /* movT R,(R)O!; movT O(R)!, R -> strT */
-		v = int32(p.To.Offset)
-
-		if v < -256 || v > 255 {
-			ctxt.Diag("offset out of range\n%v", p)
-		}
-		o1 = LD2STR(opldrpp(ctxt, int(p.As)))
-		if p.To.Type == D_XPOST {
-			o1 |= 1 << 10
-		} else {
-
-			o1 |= 3 << 10
-		}
-		o1 |= ((uint32(v) & 0x1FF) << 12) | (uint32(p.To.Reg) << 5) | uint32(p.From.Reg)
+// TODO:
+//	case 22: /* movT (R)O!,R; movT O(R)!, R -> ldrT */
+//		v = int32(p.From.Offset)
+//
+//		if v < -256 || v > 255 {
+//			ctxt.Diag("offset out of range\n%v", p)
+//		}
+//		o1 = opldrpp(ctxt, int(p.As))
+//		if p.From.Type == D_XPOST {
+//			o1 |= 1 << 10
+//		} else {
+//
+//			o1 |= 3 << 10
+//		}
+//		o1 |= ((uint32(v) & 0x1FF) << 12) | (uint32(p.From.Reg) << 5) | uint32(p.To.Reg)
+//
+//	case 23: /* movT R,(R)O!; movT O(R)!, R -> strT */
+//		v = int32(p.To.Offset)
+//
+//		if v < -256 || v > 255 {
+//			ctxt.Diag("offset out of range\n%v", p)
+//		}
+//		o1 = LD2STR(opldrpp(ctxt, int(p.As)))
+//		if p.To.Type == D_XPOST {
+//			o1 |= 1 << 10
+//		} else {
+//
+//			o1 |= 3 << 10
+//		}
+//		o1 |= ((uint32(v) & 0x1FF) << 12) | (uint32(p.To.Reg) << 5) | uint32(p.From.Reg)
 
 	case 24: /* mov/mvn Rs,Rd -> add $0,Rs,Rd or orr Rs,ZR,Rd */
 		rf = int(p.From.Reg)
@@ -2877,36 +2878,36 @@ func asmout(ctxt *obj.Link, p *obj.Prog, o *Optab, out []uint32) {
 		}
 		o2 = olsr12u(ctxt, int32(opldr12(ctxt, int(p.As))), 0, REGTMP, int(p.To.Reg))
 
-	case 66: /* ldp O(R)!, (r1, r2); ldp (R)O!, (r1, r2) */
-		v = int32(p.From.Offset)
+//	case 66: /* ldp O(R)!, (r1, r2); ldp (R)O!, (r1, r2) */
+//		v = int32(p.From.Offset)
+//
+//		if v < -512 || v > 504 {
+//			ctxt.Diag("offset out of range\n%v", p)
+//		}
+//		if p.To.Type == D_XPOST {
+//			o1 |= 1 << 23
+//		} else {
+//
+//			o1 |= 3 << 23
+//		}
+//		o1 |= 1 << 22
+//		o1 |= uint32(int64(2<<30|5<<27|((uint32(v)/8)&0x7f)<<15) | p.To.Offset<<10 | int64(uint32(p.From.Reg)<<5) | int64(p.To.Reg))
+//
+//	case 67: /* stp (r1, r2), O(R)!; stp (r1, r2), (R)O! */
+//		v = int32(p.To.Offset)
+//
+//		if v < -512 || v > 504 {
+//			ctxt.Diag("offset out of range\n%v", p)
+//		}
+//		if p.To.Type == D_XPOST {
+//			o1 |= 1 << 23
+//		} else {
+//
+//			o1 |= 3 << 23
+//		}
+//		o1 |= uint32(int64(2<<30|5<<27|((uint32(v)/8)&0x7f)<<15) | p.From.Offset<<10 | int64(uint32(p.To.Reg)<<5) | int64(p.From.Reg))
 
-		if v < -512 || v > 504 {
-			ctxt.Diag("offset out of range\n%v", p)
-		}
-		if p.To.Type == D_XPOST {
-			o1 |= 1 << 23
-		} else {
-
-			o1 |= 3 << 23
-		}
-		o1 |= 1 << 22
-		o1 |= uint32(int64(2<<30|5<<27|((uint32(v)/8)&0x7f)<<15) | p.To.Offset<<10 | int64(uint32(p.From.Reg)<<5) | int64(p.To.Reg))
-
-	case 67: /* stp (r1, r2), O(R)!; stp (r1, r2), (R)O! */
-		v = int32(p.To.Offset)
-
-		if v < -512 || v > 504 {
-			ctxt.Diag("offset out of range\n%v", p)
-		}
-		if p.To.Type == D_XPOST {
-			o1 |= 1 << 23
-		} else {
-
-			o1 |= 3 << 23
-		}
-		o1 |= uint32(int64(2<<30|5<<27|((uint32(v)/8)&0x7f)<<15) | p.From.Offset<<10 | int64(uint32(p.To.Reg)<<5) | int64(p.From.Reg))
-
-		// This is supposed to be something that stops execution.
+	// This is supposed to be something that stops execution.
 	// It's not supposed to be reached, ever, but if it is, we'd
 	// like to be able to tell how we got there.  Assemble as
 	// 0xbea71700 which is guaranteed to raise undefined instruction
