8214d4ffdabdc58a139a9809aa4403e45be292c2
Aram Hăvărneanu <aram@mgk.ro>
2015-03-30 13:46:28 +0200

    cmd/6l: use .plt instead of .got on Solaris
    Solaris requires all external procedures to be accessed through the
    PLT. If 6l won't do it, /bin/ld will, so all the code written with .GOT
    in mind won't work with the external linker.
    
    This CL makes external linking work, opening the path to cgo support
    on Solaris.
    
    This CL breaks the Solaris build, this is fixed in subsequent CLs in
    this series.
    
    Change-Id: If370a79f49fdbe66d28b89fa463b4f3e91685f69

--- src/cmd/6l/asm.go
+++ src/cmd/6l/asm.go
@@ -271,6 +271,12 @@ func adddynrel(s *ld.LSym, r *ld.Reloc) {
 
 	case ld.R_ADDR:
 		if s.Type == ld.STEXT && ld.Iself {
+			if ld.HEADTYPE == ld.Hsolaris {
+				addpltsym(targ)
+				r.Sym = ld.Linklookup(ld.Ctxt, ".plt", 0)
+				r.Add += int64(targ.Plt)
+				return
+			}
 			// The code is asking for the address of an external
 			// function.  We provide it with the address of the
 			// correspondent GOT symbol.
